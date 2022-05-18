dfb4676b6f564775d46b0ad4ae08fc9357c733b0
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-05-18 10:05:06 +0530

    Download file from local storage and handle GET requests
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from flask_wtf import FlaskForm
 from flask_wtf.csrf import CSRFProtect
 from werkzeug.utils import secure_filename
-from wtforms import FileField, SubmitField
+from wtforms import FileField, StringField, SubmitField
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
@@ -19,20 +19,37 @@ def index():
     return render_template('index.html', form=form)
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
     form = UploadForm()
-    if form.validate_on_submit():
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        path_to_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(path_to_file)
-        return render_template('upload.html', filename=filename)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            file = request.files['file']
+            filename = secure_filename(file.filename)
+            path_to_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path_to_file)
+            return render_template('upload.html', filename=filename)
+    return render_template('upload.html')
+
+
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+    form = DownloadForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            filename = request.form['text']
+            return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    return render_template('download.html', form=form)
 
 
 class UploadForm(FlaskForm):
     file = FileField('File')
     submit = SubmitField('Upload')
+
+
+class DownloadForm(FlaskForm):
+    text = StringField('Text')
+    submit = SubmitField('Download')
 
 
 if __name__ == '__main__':
