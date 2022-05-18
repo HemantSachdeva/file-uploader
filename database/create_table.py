699ebb81126df127c410aed2d3396f98d63aa233
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-05-18 10:50:32 +0530

    Drop unnecessary print statement
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- database/create_table.py
+++ database/create_table.py
@@ -7,8 +7,6 @@ def create_table():
         cursor = connection.cursor()
         cursor.execute(
             "CREATE TABLE IF NOT EXISTS uploads(id INT AUTO_INCREMENT PRIMARY KEY, file_name VARCHAR(255), URL VARCHAR(255), file_type VARCHAR(255), file_size VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)")
-        if cursor.lastrowid == 0:
-            print('Table created successfully')
         connection.commit()
         cursor.close()
         connection.close()
